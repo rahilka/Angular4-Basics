ba51937b8096eafea8c90f19ad9967d696b8a78c
Rahilka Simonova <rahilka.simonova@yahoo.com>
2017-08-23 11:58:44 +0200

    Using data with event binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -13,6 +13,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
+  serverName = '';
 
   constructor() {
   	setTimeout(() => {
@@ -25,6 +26,11 @@ export class ServersComponent implements OnInit {
 
   onCreateServer() {
   	this.serverCreationStatus = 'Server was created!';
+  }
+
+  onUpdateServerName(event: any) {
+  	this.serverName = event.target.value;
+  	// this.serverName = (<HTMLInputElement>event.target).value; ---> Explicitly say that is an input element
   }
 
 }
